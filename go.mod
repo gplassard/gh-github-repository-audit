fa3ab6662f91ad93677a56172c9e46166ef5692b
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-03-10 00:06:08 +0100

    feat(checks): add topic check

--- go.mod
+++ go.mod
@@ -7,10 +7,12 @@ require github.com/cli/go-gh v1.2.1
 require (
 	github.com/cli/safeexec v1.0.0 // indirect
 	github.com/cli/shurcooL-graphql v0.0.2 // indirect
+	github.com/fatih/color v1.14.1 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/muesli/termenv v0.12.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
--- main.go
+++ main.go
@@ -2,25 +2,63 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/pkg/api"
+	"github.com/cli/go-gh/pkg/repository"
+	"github.com/fatih/color"
+	"os"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-github-repository-audit extension!")
-	client, err := gh.RESTClient(nil)
+	repo, err := gh.CurrentRepository()
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
+	fmt.Println("Starting audit for repo", color.YellowString(repo.Owner()+"/"+repo.Name()))
+
+	valid := true
+	valid = valid && checkTeamTopic(err, repo)
+
+	if valid {
+		fmt.Println(color.GreenString("All checks have passed, congratulations ! ✅"))
+	} else {
+		fmt.Println(color.RedString("Some checks have failed ❌"))
+		os.Exit(1)
 	}
-	fmt.Printf("running as %s\n", response.Login)
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func checkTeamTopic(err error, repo repository.Repository) bool {
+	opts := api.ClientOptions{
+		Headers: map[string]string{"Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28"},
+	}
+	client, err := gh.RESTClient(&opts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	response := struct{ Names []string }{}
+	err = client.Get("repos/"+repo.Owner()+"/"+repo.Name()+"/topics", &response)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	isValid := hasValidTeamTopic(response.Names)
+	if isValid {
+		fmt.Println(color.GreenString("Repo has a valid team topic ✅"))
+	} else {
+		fmt.Println(color.RedString("Repo has no valid team topic ❌"))
+	}
+	return isValid
+}
+
+func hasValidTeamTopic(topics []string) bool {
+	acceptedTags := make(map[string]bool)
+	acceptedTags["team-gplassard"] = true
+	for _, topic := range topics {
+		if acceptedTags[topic] {
+			return true
+		}
+	}
+	return false
+}
